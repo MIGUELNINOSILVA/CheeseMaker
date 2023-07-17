6f9451803c947612a0567dba0d180594cb0040bf
MIGUELNINOSILVA <ninosilvagpt@gmail.com>
2023-07-17 08:57:11 -0500

    feat: :bug: ARREGLADO PETICIONES CON ROELS
    ROLES

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -3,8 +3,36 @@ import mongoose from "mongoose";
 const usuarioSchema = mongoose.Schema({
     nombre: {
         type: String,
+        required: [true, 'name is required'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
         required: true,
         trim: true
+    },
+    imagen: {
+        type: String
+    },
+    role:{
+        type: String,
+        required: true,
+        default: 'USER',
+        enum: ['ADMIN', 'USER']
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    googleSignIn: {
+        type: Boolean,
+        default: false
     }
 }, {
     timestamps: true
